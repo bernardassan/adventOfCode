ab1e907436451999a44ef4dce6ec55d3106dd0e9
Ultra-Code <mega.alpha100@gmail.com>
2024-11-05 17:10:49 +0000

    Complete Day08
    ensure you fully groke what is happening before commiting to master

--- zig/src/day08.zig
+++ zig/src/day08.zig
@@ -1,69 +1,75 @@
 const std = @import("std");
 const mem = std.mem;
 
-const Size = 100;
-const VisMask = std.StaticBitSet(Size);
-const Column = 100;
-const Row = 100;
 const Direction = enum { left, right, up, down };
 
-const Forest = struct {
-    const Self = @This();
-    input: [Row][Column]u8,
-    width: u32,
-    visible: VisMask,
+fn Forest(comptime size: usize) type {
+    return struct {
+        const ROW = size;
+        const COLUMN = size;
+        const VisMask = std.StaticBitSet(ROW * COLUMN);
+        const Self = @This();
+        input: [ROW][COLUMN]u8,
+        width: u32,
+        visible: VisMask,
 
-    fn init(input: []const u8) Forest {
-        var lines = mem.tokenizeScalar(u8, input, '\n');
-        const first_line = lines.next().?;
+        fn init(input: []const u8) Self {
+            var lines = mem.tokenizeScalar(u8, input, '\n');
+            const first_line = lines.next().?;
+            const grid_width = first_line.len;
 
-        const forest: Forest = .{ .input = @splat(0), .visible = VisMask.initEmpty(), .width = first_line.len };
-        @memcpy(forest.input[0], first_line);
+            var forest: Self = .{
+                .input = @splat(@splat(0)),
+                .visible = VisMask.initEmpty(),
+                .width = @intCast(grid_width),
+            };
+            @memcpy(forest.input[0][0..], first_line);
 
-        var row: usize = 0;
-        while (lines.next()) |line| : (row += 1) {
-            @memcpy(forest.input[row], line);
-        }
-        return forest;
-    }
-
-    fn nextInbounds(self: *const Self, x: usize, y: usize) bool {
-        return x > 0 and y > 0 and y < self.size - 1 and x < self.size - 1;
-    }
-
-    pub fn scoreVisible(self: *Self, x: usize, y: usize, comptime direction: Direction) u64 {
-        const tree = self.input[y][x];
-        var i: usize = x;
-        var j: usize = y;
-        var score: u64 = 0;
-        while (score < self.size) : (score += 1) {
-            switch (direction) {
-                .left => i -= 1,
-                .right => i += 1,
-                .up => j -= 1,
-                .down => j += 1,
+            var row: usize = 1; // the first line have been copied already
+            while (lines.next()) |line| : (row += 1) {
+                @memcpy(forest.input[row][0..], line);
             }
-            if (tree <= self.input[j][i]) break;
-            if (!self.nextInbounds(i, j)) {
-                self.visible.set(x + y * self.size);
-                break;
-            }
+            return forest;
         }
-        return score + 1;
-    }
-};
 
-fn solve(input: []const u8) [2]u64 {
-    const size = std.mem.indexOf(u8, input, "\n").?;
+        fn isVisible(self: *const Self, x: usize, y: usize) bool {
+            return !(x > 0 and y > 0 and x < self.width - 1 and y < self.width - 1);
+        }
 
-    var visible = try VisMask.initEmpty(size * size);
+        pub fn scoreVisible(self: *Self, x: usize, y: usize, comptime direction: Direction) u64 {
+            const tree = self.input[x][y];
+            var row: usize = x;
+            var column: usize = y;
+            var score: u64 = 0;
+            while (score < self.width) : (score += 1) {
+                switch (direction) {
+                    .left => row -= 1,
+                    .right => row += 1,
+                    .up => column -= 1,
+                    .down => column += 1,
+                }
+                if (tree <= self.input[row][column]) break;
+                if (self.isVisible(row, column)) {
+                    std.debug.print("tree {c} = {}:x {}:y dir {s}\n", .{ tree, x, y, @tagName(direction) });
+                    const column_offset = y * self.width;
+                    self.visible.set(x + column_offset);
+                    break;
+                }
+            }
+            return score + 1;
+        }
+    };
+}
 
-    const forest = Forest.init(input);
+fn solve(comptime input: []const u8) [2]u64 {
+    const grid_width = comptime mem.indexOf(u8, input, "\n").?;
+
+    var forest = Forest(grid_width).init(input);
 
     var part2: u64 = 0;
 
     var x: usize = 1;
-    const dim = size - 1;
+    const dim = grid_width - 1;
     while (x < dim) : (x += 1) {
         var y: usize = 1;
         while (y < dim) : (y += 1) {
@@ -76,16 +82,26 @@ fn solve(input: []const u8) [2]u64 {
         }
     }
 
-    const part1 = visible.count() + (4 * size) - 4;
+    const sides_count = 4;
+    // all outer trees are visible
+    const visible_outer_grid = (sides_count * grid_width) - sides_count;
+    const part1 = forest.visible.count() + visible_outer_grid;
     return .{ part1, part2 };
 }
 
-pub fn main() !void {
-    const sol = try solve(@embedFile("input.txt"));
+pub fn main() void {
+    const sol = solve(@embedFile("data/day08.txt"));
     std.debug.print("Part 1: {d}\nPart 2: {d}\n", .{ sol[0], sol[1] });
 }
 
 test "test-input" {
-    const sol = try solve(std.testing.allocator, @embedFile("test.txt"));
+    const test_input =
+        \\30373
+        \\25512
+        \\65332
+        \\33549
+        \\35390
+    ;
+    const sol = solve(test_input);
     std.debug.print("Part 1: {d}\nPart 2: {d}\n", .{ sol[0], sol[1] });
 }
